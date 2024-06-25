b3b245df2911b8593eeadfdf426bf03342d8fef0
ze-xyz <shzexyz@gmail.com>
2024-06-25 18:58:24 +0000

    Quartz sync: Jun 25, 2024, 6:58 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -7,7 +7,7 @@ const config: QuartzConfig = {
     enableSPA: false,
     enablePopovers: false,
     analytics: null,
-    baseUrl: "",
+    baseUrl: "ze-xyz/github.io/shze/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
