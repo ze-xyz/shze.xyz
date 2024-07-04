4c71ffb30838dfaae7d50b5f647ce32d99a1443c
Jose <shzexyz@gmail.com>
2024-07-04 23:38:14 +0000

    Quartz sync: Jul 4, 2024, 11:38 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,7 @@ const config: QuartzConfig = {
     theme: {
       typography: {
         header: "Computer Modern Serif",
-        body: "Computer Modern Serif",
+        body: "Computer Modern Typewriter",
         code: "Computer Modern Typewriter",
       },
       colors: {
